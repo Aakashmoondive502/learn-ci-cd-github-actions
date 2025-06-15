a79d3b90713b70741a5c52830fc862d89fe30a1b
Aakashmoondive502 <aakash.prajapati@moondive.co>
2025-06-15 23:18:45 +0530

    create-deploy-dist-second

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,10 @@ function App() {
 
   return (
     // Thi is a comment and below is a statement
-    console.log("Hello World"),
+    // console.log("Hello World"),
     <>
       <h1>Github Actions</h1>
-      <h2>Complete course with 2 projects</h2>
+      <h2>Complete course with 2 projects by Aakash</h2>
     </>
   )
 }
