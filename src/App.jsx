70ad9d25b78cc6483ba7a7223af5d5fe8217f655
Aakashmoondive502 <aakash.prajapati@moondive.co>
2025-06-15 19:09:30 +0530

    add-comments and console.logs [skip ci]

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import './App.css'
 function App() {
 
   return (
+    // Thi is a comment and below is a statement
+    console.log("Hello World"),
     <>
       <h1>Github Actions</h1>
       <h2>Complete course with 2 projects</h2>
